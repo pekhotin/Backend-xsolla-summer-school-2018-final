a4df7445823264ab847fce51cc3fe4358bfef989
tamaravedenina <tomaaaaa55555@gmail.com>
2018-09-09 16:08:12 +0000

    Update script.sql

--- script.sql
+++ script.sql
@@ -5,16 +5,43 @@ CREATE SCHEMA IF NOT EXISTS `mvc` DEFAULT CHARACTER SET utf8 ;
 USE `mvc` ;
 
 -- -----------------------------------------------------
+-- Table `mvc`.`Users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mvc`.`Users` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `login` VARCHAR(255) NOT NULL,
+  `name` VARCHAR(80) NOT NULL,
+  `surname` VARCHAR(80) NOT NULL,
+  `hash` VARCHAR(255) NOT NULL,
+  `organization` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(80) NOT NULL,
+  `phoneNumber` VARCHAR(12) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
+  UNIQUE INDEX `login_UNIQUE` (`login` ASC) VISIBLE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
 -- Table `mvc`.`Products`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mvc`.`Products` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(225) NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
   `price` FLOAT NOT NULL,
   `size` INT(11) NOT NULL,
   `type` VARCHAR(45) NOT NULL,
+  `userId` INT(11) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+  INDEX `UserProduct_idx` (`userId` ASC) VISIBLE,
+  CONSTRAINT `UserProduct`
+    FOREIGN KEY (`userId`)
+    REFERENCES `mvc`.`Users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
@@ -26,32 +53,39 @@ CREATE TABLE IF NOT EXISTS `mvc`.`Warehouses` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `address` VARCHAR(255) NOT NULL,
   `capacity` INT(11) NOT NULL,
+  `userId` INT(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
-  UNIQUE INDEX `address_UNIQUE` (`address` ASC) VISIBLE)
+  INDEX `UserWarehouse_idx` (`userId` ASC) VISIBLE,
+  CONSTRAINT `UserWarehouse`
+    FOREIGN KEY (`userId`)
+    REFERENCES `mvc`.`Users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `mvc`.`ProductBatches`
+-- Table `mvc`.`State`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mvc`.`ProductBatches` (
+CREATE TABLE IF NOT EXISTS `mvc`.`State` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `idProduct` INT(11) NOT NULL,
+  `warehouseId` INT(11) NOT NULL,
+  `productId` INT(11) NOT NULL,
   `quantity` INT(11) NOT NULL,
-  `idWarehouse` INT(11) NOT NULL,
+  `date` DATE NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
-  INDEX `idWarehouse_idx` (`idWarehouse` ASC) VISIBLE,
-  INDEX `idProduct_idx` (`idProduct` ASC) VISIBLE,
-  CONSTRAINT `idProduct`
-    FOREIGN KEY (`idProduct`)
+  INDEX `WarehouseState_idx` (`warehouseId` ASC) VISIBLE,
+  INDEX `Product_idx` (`productId` ASC) VISIBLE,
+  CONSTRAINT `Product`
+    FOREIGN KEY (`productId`)
     REFERENCES `mvc`.`Products` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `idWarehouse`
-    FOREIGN KEY (`idWarehouse`)
+  CONSTRAINT `WarehouseState`
+    FOREIGN KEY (`warehouseId`)
     REFERENCES `mvc`.`Warehouses` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
@@ -64,43 +98,26 @@ DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mvc`.`Transactions` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `idWarehouse` INT(11) NOT NULL,
-  `idProduct` INT(11) NOT NULL,
+  `warehouseId` INT(11) NOT NULL,
+  `productId` INT(11) NOT NULL,
   `quantity` INT(11) NOT NULL,
-  `direction` VARCHAR(45) NOT NULL,
-  `date` DATETIME NULL DEFAULT NULL,
+  `direction` ENUM('receipt', 'dispatch', 'betweenWarehouses') NOT NULL,
+  `datetime` DATETIME NOT NULL,
   `sender` VARCHAR(255) NULL DEFAULT NULL,
   `recipient` VARCHAR(255) NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
-  INDEX `transactionWithWarehouse_idx` (`idWarehouse` ASC) VISIBLE,
-  INDEX `transactionWithProduct_idx` (`idProduct` ASC) VISIBLE,
-  CONSTRAINT `transactionWithProduct`
-    FOREIGN KEY (`idProduct`)
+  INDEX `WarehouseTransaction_idx` (`warehouseId` ASC) VISIBLE,
+  INDEX `ProductTransaction_idx` (`productId` ASC) VISIBLE,
+  CONSTRAINT `ProductTransaction`
+    FOREIGN KEY (`productId`)
     REFERENCES `mvc`.`Products` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `transactionWithWarehouse`
-    FOREIGN KEY (`idWarehouse`)
+  CONSTRAINT `WarehouseTransaction`
+    FOREIGN KEY (`warehouseId`)
     REFERENCES `mvc`.`Warehouses` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
-
--- -----------------------------------------------------
--- Table `mvc`.`Users`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mvc`.`Users` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `surname` VARCHAR(45) NOT NULL,
-  `organization` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
-  `phoneNumber` VARCHAR(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `idUsers_UNIQUE` (`id` ASC) VISIBLE)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
